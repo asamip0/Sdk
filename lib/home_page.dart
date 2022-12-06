6b0100ef1cc095c86745404f438c82c42fc89de0
Ravikant <ravikant.paudel@gmail.com>
2022-12-06 17:37:42 +0545

    Customize card created

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -33,14 +33,12 @@ class _HomePageState extends State<HomePage> {
     final config = PaymentConfig(
       amount: 1000,
       productIdentity: 'dell-g5-g5510-2021',
-      productName:
-      'Dell G5 G5510 2021 i7 10th Gen 8 Core / RTX 3060 / 16GB RAM / 512GB SSD / 15.6" 165Hz FHD Display',
-      productUrl:
-      'https://www.sastodeal.com/dell-g5-g5510-2021-i7-10th-gen-8-core-rtx-3060-16gb-ram-512gb-ssd-15-6-165h-itti-g5510.html',
+      productName: 'Dell G5 G5510 2021 i7 10th Gen 8 Core / RTX 3060 / 16GB RAM / 512GB SSD / 15.6" 165Hz FHD Display',
+      productUrl: 'https://www.sastodeal.com/dell-g5-g5510-2021-i7-10th-gen-8-core-rtx-3060-16gb-ram-512gb-ssd-15-6-165h-itti-g5510.html',
       additionalData: {
         'vendor': 'Sastodeal',
         'image_url':
-        'https://i.dell.com/is/image/DellContent/content/dam/global-site-design/product_images/dell_client_products/notebooks/g_series/g5_15_5590-non-touch/global_spi/ng/notebook-g5-15-5590-campaign-hero-504x350-ng.psd?hei=402&qlt=90,0&op_usm=1.75,0.3,2,0&resMode=sharp&pscan=auto&fmt=pjpg',
+            'https://i.dell.com/is/image/DellContent/content/dam/global-site-design/product_images/dell_client_products/notebooks/g_series/g5_15_5590-non-touch/global_spi/ng/notebook-g5-15-5590-campaign-hero-504x350-ng.psd?hei=402&qlt=90,0&op_usm=1.75,0.3,2,0&resMode=sharp&pscan=auto&fmt=pjpg',
       },
       mobile: '9800003001',
       mobileReadOnly: false,
@@ -55,138 +53,165 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            Card(
-              child: Consumer<AppPreferenceNotifier>(
-                builder: (context, appPreference, _) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16, left: 16),
-                        child: Text(
-                          localization.appPreference.toUpperCase(),
-                          style: Theme.of(context).textTheme.overline,
-                        ),
-                      ),
-                      SwitchListTile(
-                        value: appPreference.isDarkMode,
-                        title: Text(localization.darkMode),
-                        onChanged: (isDarkMode) {
-                          context
-                              .read<AppPreferenceNotifier>()
-                              .updateBrightness(isDarkMode: isDarkMode);
-                        },
-                      ),
-                      ListTile(
-                        title: Text(localization.language),
-                        trailing: DropdownButtonHideUnderline(
-                          child: DropdownButton<Locale>(
-                            items: {
-                              'English': const Locale('en', 'US'),
-                              'नेपाली': const Locale('ne', 'NP'),
-                            }
-                                .entries
-                                .map(
-                                  (e) => DropdownMenuItem(
-                                value: e.value,
-                                child: Text(e.key),
-                              ),
-                            )
-                                .toList(growable: false),
-                            value: appPreference.locale,
-                            onChanged: (locale) {
-                              if (locale != null) {
-                                appPreference.updateLocale(locale);
-                              }
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
+            _CustomCardButton(
+              onSuccess: onSuccess,
+              onFailure: onFailure,
+              onCancel: onCancel,
+              amountInPaisa: 1000,
             ),
             const SizedBox(height: 16),
-            Row(
-              children: [
-                Flexible(
-                  child: Column(
-                    children: [
-                      KhaltiButton(
-                        config: config,
-                        onSuccess: onSuccess,
-                        onFailure: onFailure,
-                        onCancel: onCancel,
-                      ),
-                      const SizedBox(height: 8),
-                      KhaltiButton.eBanking(
-                        config: config,
-                        onSuccess: onSuccess,
-                        onFailure: onFailure,
-                        onCancel: onCancel,
-                      ),
-                      const SizedBox(height: 8),
-                      KhaltiButton.mBanking(
-                        config: config,
-                        onSuccess: onSuccess,
-                        onFailure: onFailure,
-                        onCancel: onCancel,
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Flexible(
-                  child: Column(
-                    children: [
-                      KhaltiButton.wallet(
-                        config: config,
-                        onSuccess: onSuccess,
-                        onFailure: onFailure,
-                        onCancel: onCancel,
-                      ),
-                      const SizedBox(height: 8),
-                      KhaltiButton.sct(
-                        config: config,
-                        onSuccess: onSuccess,
-                        onFailure: onFailure,
-                        onCancel: onCancel,
-                      ),
-                      const SizedBox(height: 8),
-                      KhaltiButton.connectIPS(
-                        config: config,
-                        onSuccess: onSuccess,
-                        onFailure: onFailure,
-                        onCancel: onCancel,
-                      ),
-                    ],
-                  ),
-                ),
-                if (MediaQuery.of(context).size.width > 500)
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: _CustomButton(
-                        config: config,
-                        onSuccess: onSuccess,
-                        onFailure: onFailure,
-                        onCancel: onCancel,
-                      ),
-                    ),
-                  ),
-              ],
+            _CustomCardButton(
+              onSuccess: onSuccess,
+              onFailure: onFailure,
+              onCancel: onCancel,
+              amountInPaisa: 2000,
             ),
-            if (MediaQuery.of(context).size.width < 500)
-              Padding(
-                padding: const EdgeInsets.only(top: 24),
-                child: _CustomButton(
-                  config: config,
-                  onSuccess: onSuccess,
-                  onFailure: onFailure,
-                  onCancel: onCancel,
-                ),
-              ),
+            const SizedBox(height: 16),
+            _CustomCardButton(
+              onSuccess: onSuccess,
+              onFailure: onFailure,
+              onCancel: onCancel,
+              amountInPaisa: 3000,
+            ),
+            const SizedBox(height: 16),
+            _CustomCardButton(
+              onSuccess: onSuccess,
+              onFailure: onFailure,
+              onCancel: onCancel,
+              amountInPaisa: 50000,
+            ),
+            const SizedBox(height: 16),
+            // Card(
+            //   child: Consumer<AppPreferenceNotifier>(
+            //     builder: (context, appPreference, _) {
+            //       return Column(
+            //         crossAxisAlignment: CrossAxisAlignment.start,
+            //         children: [
+            //           Padding(
+            //             padding: const EdgeInsets.only(top: 16, left: 16),
+            //             child: Text(
+            //               localization.appPreference.toUpperCase(),
+            //               style: Theme.of(context).textTheme.overline,
+            //             ),
+            //           ),
+            //           SwitchListTile(
+            //             value: appPreference.isDarkMode,
+            //             title: Text(localization.darkMode),
+            //             onChanged: (isDarkMode) {
+            //               context.read<AppPreferenceNotifier>().updateBrightness(isDarkMode: isDarkMode);
+            //             },
+            //           ),
+            //           ListTile(
+            //             title: Text(localization.language),
+            //             trailing: DropdownButtonHideUnderline(
+            //               child: DropdownButton<Locale>(
+            //                 items: {
+            //                   'English': const Locale('en', 'US'),
+            //                   'नेपाली': const Locale('ne', 'NP'),
+            //                 }
+            //                     .entries
+            //                     .map(
+            //                       (e) => DropdownMenuItem(
+            //                         value: e.value,
+            //                         child: Text(e.key),
+            //                       ),
+            //                     )
+            //                     .toList(growable: false),
+            //                 value: appPreference.locale,
+            //                 onChanged: (locale) {
+            //                   if (locale != null) {
+            //                     appPreference.updateLocale(locale);
+            //                   }
+            //                 },
+            //               ),
+            //             ),
+            //           ),
+            //         ],
+            //       );
+            //     },
+            //   ),
+            // ),
+            // const SizedBox(height: 16),
+            // Row(
+            //   children: [
+            //     Flexible(
+            //       child: Column(
+            //         children: [
+            //           KhaltiButton(
+            //             config: config,
+            //             onSuccess: onSuccess,
+            //             onFailure: onFailure,
+            //             onCancel: onCancel,
+            //           ),
+            //           const SizedBox(height: 8),
+            //           KhaltiButton.eBanking(
+            //             config: config,
+            //             onSuccess: onSuccess,
+            //             onFailure: onFailure,
+            //             onCancel: onCancel,
+            //           ),
+            //           const SizedBox(height: 8),
+            //           KhaltiButton.mBanking(
+            //             config: config,
+            //             onSuccess: onSuccess,
+            //             onFailure: onFailure,
+            //             onCancel: onCancel,
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //     const SizedBox(width: 8),
+            //     Flexible(
+            //       child: Column(
+            //         children: [
+            //           KhaltiButton.wallet(
+            //             config: config,
+            //             onSuccess: onSuccess,
+            //             onFailure: onFailure,
+            //             onCancel: onCancel,
+            //           ),
+            //           const SizedBox(height: 8),
+            //           KhaltiButton.sct(
+            //             config: config,
+            //             onSuccess: onSuccess,
+            //             onFailure: onFailure,
+            //             onCancel: onCancel,
+            //           ),
+            //           const SizedBox(height: 8),
+            //           KhaltiButton.connectIPS(
+            //             config: config,
+            //             onSuccess: onSuccess,
+            //             onFailure: onFailure,
+            //             onCancel: onCancel,
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //     if (MediaQuery.of(context).size.width > 500)
+            //       Expanded(
+            //         child: Padding(
+            //           padding: const EdgeInsets.only(left: 16),
+            //           child: _CustomButton(
+            //             config: config,
+            //             onSuccess: onSuccess,
+            //             onFailure: onFailure,
+            //             onCancel: onCancel,
+            //           ),
+            //         ),
+            //       ),
+            //   ],
+            // ),
+            // if (MediaQuery.of(context).size.width < 500)
+            //   Padding(
+            //     padding: const EdgeInsets.only(top: 24),
+            //     child: _CustomButton(
+            //       config: config,
+            //       onSuccess: onSuccess,
+            //       onFailure: onFailure,
+            //       onCancel: onCancel,
+            //     ),
+            //   ),
+            // const SizedBox(height: 16),
           ],
         ),
       ),
@@ -265,9 +290,9 @@ class _CustomButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final headline6 = Theme.of(context).textTheme.headline6?.copyWith(
-      color: Colors.pink,
-      fontWeight: FontWeight.bold,
-    );
+          color: Colors.pink,
+          fontWeight: FontWeight.bold,
+        );
 
     return InkWell(
       borderRadius: BorderRadius.circular(20),
@@ -309,6 +334,86 @@ class _CustomButton extends StatelessWidget {
                 const SizedBox(width: 8),
                 Text('PAY', style: headline6),
               ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomCardButton extends StatelessWidget {
+  const _CustomCardButton({
+    Key? key,
+    required this.onSuccess,
+    required this.onFailure,
+    required this.onCancel,
+    required this.amountInPaisa,
+  }) : super(key: key);
+
+  final ValueChanged<PaymentSuccessModel> onSuccess;
+  final ValueChanged<PaymentFailureModel> onFailure;
+  final VoidCallback? onCancel;
+  final int amountInPaisa;
+
+  @override
+  Widget build(BuildContext context) {
+    final config = PaymentConfig(
+      amount: amountInPaisa,
+      productIdentity: 'dell-g5-g5510-2021',
+      productName: 'Dell G5 G5510 2021 i7 10th Gen 8 Core / RTX 3060 / 16GB RAM / 512GB SSD / 15.6" 165Hz FHD Display',
+      productUrl: 'https://www.sastodeal.com/dell-g5-g5510-2021-i7-10th-gen-8-core-rtx-3060-16gb-ram-512gb-ssd-15-6-165h-itti-g5510.html',
+      additionalData: {
+        'vendor': 'Sastodeal',
+        'image_url':
+            'https://i.dell.com/is/image/DellContent/content/dam/global-site-design/product_images/dell_client_products/notebooks/g_series/g5_15_5590-non-touch/global_spi/ng/notebook-g5-15-5590-campaign-hero-504x350-ng.psd?hei=402&qlt=90,0&op_usm=1.75,0.3,2,0&resMode=sharp&pscan=auto&fmt=pjpg',
+      },
+      mobile: '9800003001',
+      mobileReadOnly: false,
+    );
+    final headline6 = Theme.of(context).textTheme.headline6?.copyWith(
+          color: Colors.pink,
+          fontWeight: FontWeight.bold,
+        );
+
+    return InkWell(
+      onTap: () {
+        KhaltiScope.of(context).pay(
+          config: config,
+          preferences: [
+            PaymentPreference.khalti,
+            PaymentPreference.eBanking,
+          ],
+          onSuccess: onSuccess,
+          onFailure: onFailure,
+          onCancel: onCancel,
+        );
+      },
+      child: Material(
+        color: Colors.transparent,
+
+        child: Center(
+          child: Card(
+            child: Consumer<AppPreferenceNotifier>(
+              builder: (context, appPreference, _) {
+                return Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('1'),
+                          const SizedBox(width: 4),
+                          Text('First Product', style: Theme.of(context).textTheme.bodyText2),
+                          const Spacer(),
+                          Text('Rs: ${amountInPaisa / 100}'),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ),
